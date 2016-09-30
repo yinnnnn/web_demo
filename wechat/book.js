258455475fd43596a61af27776c67061b57811da
vernonyin <137664195@qq.com>
2016-10-01 00:14:39 +0800

    book

--- wechat/book.js
+++ wechat/book.js
@@ -9,25 +9,35 @@ $(document).ready(function(){
 
 
 var vm = new Vue({
-    el: '#app',
-    data: {
-        book_list:[{'name':"xxx",'url':"baidu.com"}]
-    },
-    methods: {
-        change: function (type) {
-            
-        }
-    },
+	el: '#app',
+	data: {
+		book_list:[{'name':"xxx",'url':"baidu.com"}]
+	},
+	methods: {
+		change1: function (type) {
+			console.log(type);
+			var _self = this;
+		$.ajax({
+			url: "http://120.25.78.52/wechat/autoresponse.php?type="+type,
+			async:false,
+			dataType: 'json',
+			type: 'GET',
+			success: function (res) {
+				_self.book_list = res; 
+			}
+		});
+		}
+	},
 	created: function () {
 		var _self = this;
-		$.ajax({
-		url: "http://120.25.78.52/wechat/autoresponse.php",
-		async:false,
-		dataType: 'json',
-		type: 'GET',
-		success: function (res) {
-			_self.book_list = res; 
-		}
-	});
-  }
+		// $.ajax({
+		// 	url: "http://120.25.78.52/wechat/autoresponse.php",
+		// 	async:false,
+		// 	dataType: 'json',
+		// 	type: 'GET',
+		// 	success: function (res) {
+		// 		_self.book_list = res; 
+		// 	}
+		// });
+	}
 })
